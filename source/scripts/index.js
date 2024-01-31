0ad5f56f360f367aeb29dbf473ff4cc0cc42a752
Александр <alex.zemtsov2014@yandex.ru>
2024-01-31 23:55:22 +0300

    Добавляет адаптив

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,21 @@
 /* в этот файл добавляет скрипты*/
+
+const navMain = document.querySelector('.main-nav__list');
+const navToggle = document.querySelector('.main-header__toggle-menu');
+
+navMain.classList.remove('main-nav__list--nojs');
+navToggle.classList.remove('main-header__toggle-menu--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav__list--opened')) {
+    navMain.classList.remove('main-nav__list--opened');
+    navMain.classList.add('main-nav__list--closed');
+    navToggle.classList.toggle('main-header__toggle-menu--closes');
+    navToggle.classList.toggle('main-header__toggle-menu--opens');
+  } else {
+    navMain.classList.add('main-nav__list--opened');
+    navMain.classList.remove('main-nav__list--closed');
+    navToggle.classList.toggle('main-header__toggle-menu--opens');
+    navToggle.classList.toggle('main-header__toggle-menu--closes');
+  }
+});
